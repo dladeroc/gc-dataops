4f569ee913c91595f237560fbb42b9dadbbaa3c2
Carlos Oliveira <cdoliveirar@gmail.com>
2020-08-18 11:20:29 -0500

    adding diff file

--- jenkins/jenkinsfile-diff.groovy
+++ jenkins/jenkinsfile-diff.groovy
@@ -17,14 +17,14 @@ node {
             steps.archiveArtifacts(artifacts:'**/*.gz')
         }
         
-        stage('..::SnapShot::..') {
+        stage('..::DIFF::..') {
             String credentialId="user-password-database-azure"
             String liquibaseImage="gc/liquibase-4.0.0:1.0"
             steps.withCredentials([
                  [$class: "UsernamePasswordMultiBinding", credentialsId: credentialId, usernameVariable: 'databaseUser', passwordVariable: 'databasePassword']
                  ]) {
                      try{
-                         String liquibaseExec="liquibase --url='jdbc:sqlserver://liquibase.database.windows.net:1433;database=liquibase_dev;' --changeLogFile=/src/changelog.xml --username='${env.databaseUser}' --password='${env.databasePassword}' --logLevel=info --outputFile=myschemaSnapshot.json --snapshotFormat=json snapshot"
+                         String liquibaseExec="liquibase --url='jdbc:sqlserver://liquibase.database.windows.net:1433;database=liquibase_dev;' --changeLogFile=/src/changelog.xml --username='${env.databaseUser}' --password='${env.databasePassword}' --logLevel=info snapshot"
 
                          steps.echo "liquibaseExec: ${liquibaseExec}"
                          steps.sh "docker run --rm --network=host -v '${env.WORKSPACE}:/home/liquibase/'  ${liquibaseImage} ${liquibaseExec}"
@@ -38,4 +38,4 @@ node {
     } catch(Exception e) {
         throw e
     }
-}
+}sder
