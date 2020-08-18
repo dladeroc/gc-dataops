07e8f9f74cd0881ebe5cccd555c86dd959442d28
Carlos Oliveira <cdoliveirar@gmail.com>
2020-08-17 19:07:49 -0500

    cmbios

--- jenkins/snapshot.groovy
+++ jenkins/snapshot.groovy
@@ -23,7 +23,24 @@ node {
                  [$class: "UsernamePasswordMultiBinding", credentialsId: credentialId, usernameVariable: 'databaseUser', passwordVariable: 'databasePassword']
                  ]) {
                      try{
-                         String liquibaseExec="liquibase --url='jdbc:sqlserver://liquibase.database.windows.net:1433;database=liquibase_dev;' --changeLogFile=/src/changelog.xml --username='${env.databaseUser}' --password='${env.databasePassword}' --logLevel=info update"
+                         String liquibaseExec="liquibase --url='jdbc:sqlserver://liquibase.database.windows.net:1433;database=liquibase_db;' --changeLogFile=/src/changelog.xml --username='${env.databaseUser}' --password='${env.databasePassword}' --logLevel=info update"
+
+                         steps.echo "liquibaseExec: ${liquibaseExec}"
+                         steps.sh "docker run --rm --network=host -v '${env.WORKSPACE}:/home/liquibase/'  ${liquibaseImage} ${liquibaseExec}"
+                    } catch (Exception e) {
+                         throw e
+                    }
+            }
+        }
+
+        stage('..::SnapShot::..') {
+            String credentialId="user-password-database-azure"
+            String liquibaseImage="gc/liquibase-4.0.0:1.0"
+            steps.withCredentials([
+                 [$class: "UsernamePasswordMultiBinding", credentialsId: credentialId, usernameVariable: 'databaseUser', passwordVariable: 'databasePassword']
+                 ]) {
+                     try{
+                         String liquibaseExec="liquibase --url='jdbc:sqlserver://liquibase.database.windows.net:1433;database=liquibase_db;' --changeLogFile=/src/changelog.xml --username='${env.databaseUser}' --password='${env.databasePassword}' --logLevel=info snapshot"
 
                          steps.echo "liquibaseExec: ${liquibaseExec}"
                          steps.sh "docker run --rm --network=host -v '${env.WORKSPACE}:/home/liquibase/'  ${liquibaseImage} ${liquibaseExec}"
